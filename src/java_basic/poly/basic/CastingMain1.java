1095239a04628b54621e867a26ca193821e13ddb
gyueunnim <rnrmfrbdms@gmail.com>
2024-04-02 18:53:51 +0900

    add: java-basic 10.다형성1

---
+++ src/java_basic/poly/basic/CastingMain1.java
@@ -0,0 +1,13 @@
+package java_basic.poly.basic;
+
+public class CastingMain1 {
+    public static void main(String[] args) {
+        Parent poly = new Child();
+        // 단 자식의 기능은 호출할 수 없다.
+//        poly.childMethod();
+
+        // 다운캐스팅(부모 타입 -> 자식 타입)
+        Child child = (Child)poly;
+        child.childMethod();
+    }
+}
